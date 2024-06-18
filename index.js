d3e2804d8a49432da7c7a587d2b296c677019896
unknown <leticiatiney@gmail.com>
2024-06-17 21:41:36 -0600

    ConfiguracionCors

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const app = express();
 const port = 4200;
 
 
-app.use(
+app.use(cors({ origin: true })); // Allow all origins
+app.use(cors({ credentials: true }))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/*app.use(
   cors({
     origin: "http://localhost:3000",
     credentials: true,
@@ -18,7 +24,7 @@ app.use(
 );
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));*/
 
 // Use routes
 app.use("/", routes);
@@ -27,3 +33,27 @@ app.use("/", routes);
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+
+
+/*
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+// Rutas y controladores
+app.get('/Books', (req, res) => {
+  // Lógica de tu endpoint
+});
+
+app.listen(3000, () => {
+  console.log('Servidor escuchando en puerto 3000');
+});*/
+
